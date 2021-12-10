b320313fa66de7a508baf638d89297ef0cd1a07a
Tsgzj <swx@tsgzj.me>
2021-12-09 21:51:59 -0800

    Start day10

---
+++ src/day10.clj
@@ -0,0 +1,8 @@
+(ns day10
+  :require [clojure.java.io :as io]
+  [clojure.pprint :as pp]
+  [clojure.string :as str])
+
+(defn get-lines [fname]
+  (with-open [r (io/reader fname)]
+    (doall (line-seq r))))
