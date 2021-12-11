352f369ed7aede87086d29e88e6fcb8803c9da31
Wen Sun <wenxsun@ebay.com>
2021-12-10 19:06:58 -0800

    Day 10 WIP

--- src/day10.clj
+++ src/day10.clj
@@ -1,8 +1,11 @@
 (ns day10
-  :require [clojure.java.io :as io]
-  [clojure.pprint :as pp]
-  [clojure.string :as str])
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]))
 
 (defn get-lines [fname]
   (with-open [r (io/reader fname)]
     (doall (line-seq r))))
+
+(def parens
+  {\) \( \} \{ \] \[ } )
