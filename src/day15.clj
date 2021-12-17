3ba63fc67879a450df99026558a1d4c2f4d6b588
Tsgzj <wxsun1991@gmail.com>
2021-12-17 12:26:21 -0800

    Day 15 done

--- src/day15.clj
+++ src/day15.clj
@@ -10,6 +10,10 @@
 
 (def file (get-lines "input/day15"))
 
+(def height (count file))
+
+(def width (count file))
+
 (defn parse-grid [lines]
   (into {}
         (for [[j line] (map-indexed vector lines)
@@ -18,14 +22,68 @@
 
 (def grid (parse-grid file))
 
-(defn neighbours [grid [i j]]
-  (filter #(grid %)
-          [[(inc i) j] [(dec i) j] [i (inc j)] [i (dec j)]]))
+(defn neighbours [m x]
+  (let [[i j] x]
+    (filter #(m %)
+            [[(inc i) j] [(dec i) j] [i (inc j)] [i (dec j)]])))
 
-(defn heuristic [p]
+(defn edges [m a b]
+  (m b))
+
+(defn heuristic [p goal]
   ;; Since all the edges will be larger than 0 it's safe to estimate
   ;; the heuristic function to be the distance to goal
-  (let [[i j] p]
-    (+ (- 100 i) (- 100 j))))
+  (let [[i j] p
+        [gi gj] goal]
+    (+ (- gi i) (- gj j))))
 
-(defn A* [graph heuristic start goal])
+(defn A* [edges neighbours heuristic start goal]
+  (loop [q (priority-map start (heuristic start goal))
+         preds {}
+         shortest {start 0}
+         done #{}]
+    (when-let [[x hx] (peek q)]
+      (if (= x goal)
+        (reverse (take-while identity (iterate preds goal)))
+        (let [dx (- hx (heuristic x goal))
+              bn (for [n (remove done (neighbours x))
+                       :let [hn (+ dx (edges x n) (heuristic x goal))
+                             sn (shortest n Double/POSITIVE_INFINITY)]
+                       :when (< hn sn)]
+                   [n hn])]
+          (recur (into (pop q) bn)
+                 (into preds (for [[n] bn] [n x]))
+                 (into shortest bn)
+                 (conj done x)))))))
+
+
+(defn chiton [m start goal]
+  (A* (partial edges m) (partial neighbours m) heuristic start goal))
+
+(defn enlarge [g n]
+  (into {}
+        (for [i (range (* n width))
+              j (range (* n height))]
+          (let [ni (mod i width)
+                nj (mod j height)
+                v (+ (g [ni nj]) (+ (quot i width) (quot j height)))
+                nv (mod (+ v (quot v 10)) 10)]
+            [[i j] nv]))))
+
+(def lgrid (enlarge grid 5))
+
+(def part1
+  (- (reduce + (map #(grid %)
+                    (chiton grid [0 0] [(dec width) (dec height)])))
+     (grid [0 0])))
+
+(def part2
+  (- (reduce + (map #(lgrid %)
+                    (chiton lgrid [0 0] [(dec (* 5 width)) (dec (* 5 height))])))
+     (lgrid [0 0])))
+
+(defn solve [opts]
+  (pp/pprint (format "Problem one: %d" part1))
+  (pp/pprint (format "Problem two: %d" part2)))
+
+(solve nil)
