11a54fcdbfa43198d5d50ba6c4de410914cdda0c
Tsgzj <swx@tsgzj.me>
2022-12-06 22:20:30 -0800

    Day 4 part 2

--- 2022/src/Day4.hs
+++ 2022/src/Day4.hs
@@ -15,10 +15,19 @@ parseLine = parseOn (splitOn ",") parsePair
 include :: ((Int, Int), (Int, Int)) -> Bool
 include ((a, b), (c, d)) = a <=c && b >= d ||a >= c && b <= d
 
+overlap :: ((Int, Int), (Int, Int)) -> Bool
+overlap ((a, b), (c, d)) = r - l >= 0
+  where l = max a c
+        r = min b d 
+
 part1 :: [String] -> Int
 part1 = length . filter (include . parseLine)
+
+part2 :: [String] -> Int
+part2 = length . filter (overlap . parseLine)
 
 main :: IO ()
 main = do
   input <- lines <$> readFile "./../input/Day4.txt"
   print $ part1 input
+  print $ part2 input
